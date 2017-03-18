4556915c395958d579b24ac7c3063ecb50ba3eb1
Michael Ries <michael@riesd.com>
2017-03-17 21:18:06 -0600

    add hex attributes

--- mix.exs
+++ mix.exs
@@ -2,12 +2,19 @@ defmodule SsdpAutoConnect.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ssdp_auto_connect,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :ssdp_auto_connect,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      licenses: ["MIT"],
+      maintainers: ["Michael Ries"],
+      links: %{
+        "github" => "https://github.com/mmmries/ssdp_auto_connect",
+      },
+    ]
   end
 
   def application do
